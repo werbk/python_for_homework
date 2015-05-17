3b401c7ae23f2c0492391127fa61289d5afc9908
werbk <werbk@bk.ru>
2015-05-17 18:45:46 +0300

    Fix test

--- first_task.py
+++ first_task.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+import nose
 
 
 def is_alert_present(wd):
@@ -11,12 +12,14 @@ def is_alert_present(wd):
         return False
 
 
-class name(unittest.TestCase):
+class TestGroup(unittest.TestCase):
+
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_(self):
+    def test_create_group(self):
+        """Validation of correct create test group"""
         success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
@@ -41,11 +44,19 @@ class name(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_css_selector("div.msgbox").click()
         wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
     
     def tearDown(self):
+        success = True
+        # We should delete all created test form
+        self.wd.find_element_by_link_text("groups").click()
+        self.wd.find_element_by_css_selector("span.group").click()
+        if not self.wd.find_element_by_name("selected[]").is_selected():
+            self.wd.find_element_by_name("selected[]").click()
+        self.wd.find_element_by_xpath("//div[@id='content']/form/input[5]").click()
+        self.wd.find_element_by_link_text("Logout").click()
+        self.assertTrue(success)
         self.wd.quit()
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    nose.run(argv=["nosetests", "first_task.py", "--verbosity=2"])
