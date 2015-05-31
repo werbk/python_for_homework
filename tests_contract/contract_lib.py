ccff47a528881be1a54ac3e92ecbe548cb6c3b16
werbk <werbk@bk.ru>
2015-05-31 23:48:08 +0300

    add validation

--- tests_contract/contract_lib.py
+++ tests_contract/contract_lib.py
@@ -1,5 +1,6 @@
 from fixture.variables import UserLogin
 from functools import wraps
+from fixture.variables import Profinity
 
 
 def connection(fn):
@@ -14,7 +15,7 @@ def connection(fn):
     return wrapper
 
 
-class Contract:
+class Contact:
     def __init__(self, first_name=None, middle_name=None, last_name=None, nickname=None, title=None, company_name=None,
                  address_name=None, home=None, mobile=None, work=None, fax=None, email1=None, email2=None, email3=None,
                  homepage=None, address=None, phone=None, notes=None, add_year=None):
@@ -44,82 +45,95 @@ class ContactBase():
     def __init__(self, app):
         self.app = app
 
-    def create(self, Contract):
+    def count(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys()
+        wd.find_element_by_link_text("home").click()
+        return len(wd.find_elements_by_name("selected[]"))
 
-        if Contract.first_name:
+    def validation_of_contact_exist(self):
+        if self.count() == 0:
+            self.create((Contact(first_name=Profinity.correct_data, last_name=Profinity.correct_data,
+                        middle_name=Profinity.correct_data, nickname=Profinity.correct_data)))
+
+    def edit_contract(self, Contract):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_css_selector("img[alt=\"Edit\"]").click()
+        self.contract_field(Contract)
+        wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
+        wd.find_element_by_link_text("home page").click()
+
+    def contract_field(self, Contact):
+        wd = self.app.wd
+        if Contact.first_name:
             wd.find_element_by_name("firstname").click()
             wd.find_element_by_name("firstname").clear()
-            wd.find_element_by_name("firstname").send_keys("%s" % Contract.first_name)
-        if Contract.middle_name:
+            wd.find_element_by_name("firstname").send_keys("%s" % Contact.first_name)
+        if Contact.middle_name:
             wd.find_element_by_name("middlename").click()
             wd.find_element_by_name("middlename").clear()
-            wd.find_element_by_name("middlename").send_keys("%s" % Contract.middle_name)
-        if Contract.last_name:
+            wd.find_element_by_name("middlename").send_keys("%s" % Contact.middle_name)
+        if Contact.last_name:
             wd.find_element_by_name("lastname").click()
             wd.find_element_by_name("lastname").clear()
-            wd.find_element_by_name("lastname").send_keys("%s" % Contract.last_name)
-        if Contract.nickname:
+            wd.find_element_by_name("lastname").send_keys("%s" % Contact.last_name)
+        if Contact.nickname:
             wd.find_element_by_name("nickname").click()
             wd.find_element_by_name("nickname").clear()
-            wd.find_element_by_name("nickname").send_keys("%s" % Contract.nickname)
+            wd.find_element_by_name("nickname").send_keys("%s" % Contact.nickname)
 
-        if Contract.title:
+        if Contact.title:
             wd.find_element_by_name("title").click()
             wd.find_element_by_name("title").clear()
-            wd.find_element_by_name("title").send_keys("%s" % Contract.title)
+            wd.find_element_by_name("title").send_keys("%s" % Contact.title)
 
-        if Contract.company_name:
+        if Contact.company_name:
             wd.find_element_by_name("company").click()
             wd.find_element_by_name("company").clear()
-            wd.find_element_by_name("company").send_keys("%s" % Contract.company_name)
+            wd.find_element_by_name("company").send_keys("%s" % Contact.company_name)
 
-        if Contract.address_name:
+        if Contact.address_name:
             wd.find_element_by_name("address").click()
             wd.find_element_by_name("address").clear()
-            wd.find_element_by_name("address").send_keys("%s" % Contract.address_name)
+            wd.find_element_by_name("address").send_keys("%s" % Contact.address_name)
 
-        if Contract.home:
+        if Contact.home:
             wd.find_element_by_name("home").click()
             wd.find_element_by_name("home").clear()
-            wd.find_element_by_name("home").send_keys("%s" % Contract.home)
-        if Contract.mobile:
+            wd.find_element_by_name("home").send_keys("%s" % Contact.home)
+        if Contact.mobile:
             wd.find_element_by_name("mobile").click()
             wd.find_element_by_name("mobile").clear()
-            wd.find_element_by_name("mobile").send_keys("%s" % Contract.mobile)
-        if Contract.work:
+            wd.find_element_by_name("mobile").send_keys("%s" % Contact.mobile)
+        if Contact.work:
             wd.find_element_by_name("work").click()
             wd.find_element_by_name("work").clear()
-            wd.find_element_by_name("work").send_keys("%s" % Contract.work)
-        if Contract.fax:
+            wd.find_element_by_name("work").send_keys("%s" % Contact.work)
+        if Contact.fax:
             wd.find_element_by_name("fax").click()
             wd.find_element_by_name("fax").clear()
-            wd.find_element_by_name("fax").send_keys("%s" % Contract.fax)
+            wd.find_element_by_name("fax").send_keys("%s" % Contact.fax)
 
-        if Contract.email1:
+        if Contact.email1:
             wd.find_element_by_name("email").click()
             wd.find_element_by_name("email").click()
             wd.find_element_by_name("email").clear()
-            wd.find_element_by_name("email").send_keys("%s" % Contract.email1)
-        if Contract.email2:
+            wd.find_element_by_name("email").send_keys("%s" % Contact.email1)
+        if Contact.email2:
             wd.find_element_by_name("email2").click()
             wd.find_element_by_name("email2").clear()
-            wd.find_element_by_name("email2").send_keys("%s" % Contract.email2)
-        if Contract.email3:
+            wd.find_element_by_name("email2").send_keys("%s" % Contact.email2)
+        if Contact.email3:
             wd.find_element_by_name("email3").click()
             wd.find_element_by_name("email3").clear()
-            wd.find_element_by_name("email3").send_keys("%s" % Contract.email3)
+            wd.find_element_by_name("email3").send_keys("%s" % Contact.email3)
 
-        if Contract.homepage:
+        if Contact.homepage:
             wd.find_element_by_name("homepage").click()
             wd.find_element_by_name("homepage").clear()
-            wd.find_element_by_name("homepage").send_keys("%s" % Contract.homepage)
+            wd.find_element_by_name("homepage").send_keys("%s" % Contact.homepage)
 
-        if Contract.add_year:
+        if Contact.add_year:
             # in futures we can made function where we will sent date and it choose it with similar way as previous
             if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
                 wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
@@ -136,20 +150,27 @@ class ContactBase():
             wd.find_element_by_name("ayear").clear()
             wd.find_element_by_name("ayear").send_keys("1999")
 
-        if Contract.address:
+        if Contact.address:
             wd.find_element_by_name("address2").click()
             wd.find_element_by_name("address2").clear()
-            wd.find_element_by_name("address2").send_keys("%s" % Contract.address)
+            wd.find_element_by_name("address2").send_keys("%s" % Contact.address)
 
-        if Contract.phone:
+        if Contact.phone:
             wd.find_element_by_name("phone2").click()
             wd.find_element_by_name("phone2").clear()
-            wd.find_element_by_name("phone2").send_keys("%s" % Contract.phone)
+            wd.find_element_by_name("phone2").send_keys("%s" % Contact.phone)
 
-        if Contract.notes:
+        if Contact.notes:
             wd.find_element_by_name("notes").click()
             wd.find_element_by_name("notes").clear()
-            wd.find_element_by_name("notes").send_keys("%s" % Contract.notes)
+            wd.find_element_by_name("notes").send_keys("%s" % Contact.notes)
+
+
+    def create(self, Contact):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+        self.contract_field(Contact)
 
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -159,7 +180,6 @@ class ContactBase():
 
         if not wd.find_element_by_name("selected[]").is_selected():
             wd.find_element_by_name("selected[]").click()
-
 
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
--- tests_group/test_group.py
+++ tests_group/test_group.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 from group_lib import Group
-from tests_contract.contract_lib import connection
+from fixture.variables import Profinity
 
 
-@connection
 def test_create_group(app):
     """Validation of correct create test group (All field fill up)"""
 
-
-    app.group.create(Group(group_name='test', group_header='test', group_footer='test'))
+    app.group.create(Group(group_name=Profinity.correct_data, group_header=Profinity.correct_data,
+                           group_footer=Profinity.correct_data))
     app.group.click_group_page()
     app.group.delete_first_group()
 
 
-@connection
 def test_create_group_name(app):
     """Validation of correct create test group (Only group name fill up)"""
 
@@ -22,13 +20,17 @@ def test_create_group_name(app):
     app.group.delete_first_group()
 
 
-@connection
 def test_edit_group(app):
     """Validation of correct edit group (all field updated)"""
 
-    app.group.create(Group(group_name='test'))
-    app.group.click_group_page()
-    app.group.edit_group(Group(group_name='testtest', group_header='testtest', group_footer='testtest'))
+    app.group.validation_of_group_exist()
+    app.group.edit_group(Group(group_name=Profinity.long_word_20, group_header=Profinity.long_word_20,
+                               group_footer=Profinity.long_word_20))
     app.group.delete_first_group()
 
+
+def test_delete_group(app):
+    """Validation of correct delete group"""
+    app.group.validation_of_group_exist()
+    app.group.delete_first_group()
 
