24a491285fd874a069cbd33974670d0d1a6b8e1d
werbk <werbk@bk.ru>
2015-08-09 19:00:11 +0300

    task 10

--- conftest.py
+++ conftest.py
@@ -1,62 +1,33 @@
 import pytest
-import json
-import os.path
-from fixture.application import Application
-import ftputil
+import logging
+
+from fixture.TestBase import BaseClass
+from fixture.variables import UserLogin
+
 
 fixture = None
-target = None
 
-
-def install_server_configuration(host, username, password):
-    with ftputil.FTPHost(host, username, password) as remote:
-        if remote.path.isfile("config_inc.php.bak"):
-            remote.remove("config_inc.php.bak")
-        if remote.path.isfile("config_inc.php"):
-            remote.rename("config_inc.php", "config_inc.php.bak")
-        remote.upload(os.path.join(os.path.dirname(__file__),"resources/config_inc.php"), "config_inc.php")
-
-
-def restore_server_configurtion(host, username, password):
-    with ftputil.FTPHost(host, username, password) as remote:
-        if remote.path.isfile("config_inc.php.bak"):
-            if remote.path.isfile("config_inc.php"):
-                remote.remove("config_inc.php")
-            remote.rename("config_inc.php.bak", "config_inc.php")
-
-
-def load_config(file):
-    global target
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
-        with open(config_file) as f:
-            target = json.load(f)
-    return target
 
 @pytest.fixture
-def app(request, config):
+def app(request):
     global fixture
-    browser = request.config.getoption("--browser")
-    if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, config=config)
-    fixture.session.ensure_login(username=config['webadmin']['username'], password=config['webadmin']['password'])
+    browser = request.config.getoption('--browser')
+    url = request.config.getoption('--baseUrl')
+    loginu = request.config.getoption('--loginu')
+    loginp = request.config.getoption('--loginp')
+    if fixture is None:
+        fixture = BaseClass(browser=browser, base_url = url)
+
+    else:
+        if not fixture.is_valid():
+            fixture = BaseClass(browser=browser, base_url = url)
+
+    fixture.session.ensure_login(user_name=loginu, password=loginp)
     return fixture
 
-
-@pytest.fixture(scope="session")
-def config(request):
-    return load_config(request.config.getoption("--target"))
-
-@pytest.fixture(scope="session", autouse=True)
-def configure_server(request, config):
-    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
-    def fin():
-        restore_server_configurtion(config['ftp']['host'],config['ftp']['username'],config['ftp']['password'])
-    request.addfinalizer(fin)
-
-
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def stop(request):
+
     def fin():
         fixture.session.ensure_logout()
         fixture.restore()
@@ -64,8 +35,11 @@ def stop(request):
     return fixture
 
 
-
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json")
+    default_login_user = [UserLogin.name, UserLogin.password]
+    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--baseUrl', action='store', default='http://localhost/addressbook/')
 
+    # i believe that it possible do in 1 line but i don't know how to in 1 Login take to parameter at same time
+    parser.addoption('--loginu', action='store', default=default_login_user[0])
+    parser.addoption('--loginp', action='store', default=default_login_user[1])
